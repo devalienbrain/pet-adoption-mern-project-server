fcb3865288f7f5c0f2b5caf0bf88f2cbf1800fd6
Alien Brain <murad.dub@gmail.com>
2023-11-25 18:04:53 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`PawsPalace-pet adoption SERVER running on port: ${port}`);
 });
+
+// MongoDB Starts Here
+
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+console.log(process.env.DB_USER, process.env.DB_PASS);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m38robg.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged. Successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
